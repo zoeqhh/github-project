8f9aecd57b3c19c5e4d3378037f5102be38a9b8b
zoe <zoe.qin@silksoftware.com>
2022-04-20 18:14:20 +0800

    add store dir

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {Switch,Route,Redirect} from 'react-router-dom'
-import {DashBoard,Detail} from './pages'
+// import {DashBoard,Detail} from './pages'
 import {MyHeader} from './components'
 import routes from './routers'
 
